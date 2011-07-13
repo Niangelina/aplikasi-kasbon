e3a6c3ab51495b83f09e679ed1489d397bcabb47
Fanani M. Ihsan <contact@fanani.net>
2011-07-13 10:59:19 +0700

    delete view milik aplikasi billing

--- aplikasi-kasbon-ui-web/src/main/webapp/application/views/m_account_view.php
+++
@@ -1,7 +0,0 @@
-<?php $this->load->view('example1/header_view'); ?>
-
-<?=$crud;?>
-
-<?php echo anchor('welcome','Back','title="'.t('Back To Home').'"'); ?>
-
-<?php $this->load->view('example1/footer_view'); ?>
