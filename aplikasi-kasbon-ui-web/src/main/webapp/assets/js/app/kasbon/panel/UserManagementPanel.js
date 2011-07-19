3c42b7352ac00dc8aba45905c548a7c5ef796b97
Fanani M. Ihsan <contact@fanani.net>
2011-07-19 15:54:16 +0700

    membuat implement button untuk usermanagement

--- aplikasi-kasbon-ui-web/src/main/webapp/assets/js/app/kasbon/panel/UserManagementPanel.js
+++ aplikasi-kasbon-ui-web/src/main/webapp/assets/js/app/kasbon/panel/UserManagementPanel.js
@@ -13,19 +13,67 @@ Ext.define('kasbon.panel.UserManagementPanel', {
     width: 200,
     initComponent:function(){
         this.items = [
-        {
-            xtype: 'kasbon.form.UserManagementForm'
-        },
-        {
-            xtype: 'kasbon.grid.UserManagementGrid', 
-            region: 'center'
-        }
+            {
+                xtype: 'kasbon.form.UserManagementForm'
+            },
+            {
+                xtype: 'kasbon.grid.UserManagementGrid', 
+                region: 'center'
+            }
         ];
         
         this.tbar = {
-            xtype: 'kasbon.toolbar.CrudToolbar'
+            xtype: 'kasbon.toolbar.CrudToolbar',
+            listeners: {
+                scope: this, 
+                onCrudToolbarAdd: this.newUser,
+                onCrudToolbarEdit: this.editUser,
+                onCrudToolbarDelete: this.deleteUser,
+                onCrudToolbarSave: this.saveUser, 
+                onCrudToolbarCancel: this.cancelUser
+            }
           
         };
         kasbon.panel.UserManagementPanel.superclass.initComponent.call(this);
+    },
+    newUser: function(){
+        //        Ext.getCmp('billing.form.UserManagementForm').getForm().reset();
+        //        Ext.getCmp('billing.form.UserManagementForm').enableUserManagementForm();
+        Ext.MessageBox.alert(
+        'Error!',
+        'Implement button add'
+    )
+    },
+    editUser: function(){
+        //        Ext.getCmp('billing.form.UserManagementForm').getForm().reset();
+        //        Ext.getCmp('billing.form.UserManagementForm').enableUserManagementForm();
+        Ext.MessageBox.alert(
+        'Error!',
+        'Implement button Edit'
+    )
+    },
+    deleteUser: function(){
+        //        Ext.getCmp('billing.form.UserManagementForm').getForm().reset();
+        //        Ext.getCmp('billing.form.UserManagementForm').enableUserManagementForm();
+        Ext.MessageBox.alert(
+        'Error!',
+        'Implement button delete'
+    )
+    },
+    saveUser: function(){
+        //        Ext.getCmp('billing.form.UserManagementForm').getForm().reset();
+        //        Ext.getCmp('billing.form.UserManagementForm').enableUserManagementForm();
+        Ext.MessageBox.alert(
+        'Error!',
+        'Implement button save'
+    )
+    },
+    cancelUser: function(){
+        //        Ext.getCmp('billing.form.UserManagementForm').getForm().reset();
+        //        Ext.getCmp('billing.form.UserManagementForm').enableUserManagementForm();
+        Ext.MessageBox.alert(
+        'Error!',
+        'Implement button cancel'
+    )
     }
 });
