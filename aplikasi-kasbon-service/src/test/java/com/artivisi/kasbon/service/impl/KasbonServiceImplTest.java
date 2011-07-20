fb409d1a22ffa417fb25c2e098e16a506ad93520
Endy Muhardin <endy.muhardin@gmail.com>
2011-07-20 14:18:29 +0700

    truncate kena constraint, ganti menjadi delete from

--- aplikasi-kasbon-service/src/test/java/com/artivisi/kasbon/service/impl/KasbonServiceImplTest.java
+++ aplikasi-kasbon-service/src/test/java/com/artivisi/kasbon/service/impl/KasbonServiceImplTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2011 ArtiVisi Intermedia <info@artivisi.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.artivisi.kasbon.service.impl;
 
 import java.math.BigDecimal;
@@ -30,7 +45,7 @@ public class KasbonServiceImplTest {
 
 	@Before
 	public void setUp() throws Exception {
-		String sql = "truncate m_karyawan";
+		String sql = "delete from m_karyawan";
 		Connection conn = dataSource.getConnection();
 		conn.createStatement().executeUpdate(sql);
 		conn.close();
